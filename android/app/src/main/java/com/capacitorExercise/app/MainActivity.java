8bacb48cda499f4db30f6b4a530bfe838ce150bb
kartikr <rkartik1992@gmail.com>
2020-02-06 01:58:52 +0530

    Add secure flag for preventing screenshot.

--- android/app/src/main/java/com/capacitorExercise/app/MainActivity.java
+++ android/app/src/main/java/com/capacitorExercise/app/MainActivity.java
@@ -1,6 +1,7 @@
 package com.capacitorExercise.app;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import com.getcapacitor.BridgeActivity;
 import com.getcapacitor.Plugin;
@@ -11,6 +12,9 @@ public class MainActivity extends BridgeActivity {
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+
+    // set secure flags
+    getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE);
 
     // Initializes the Bridge
     this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
